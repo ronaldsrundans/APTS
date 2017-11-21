a2a2d67af968c522b42650cc1197f24375734dcb
Ronny <ronaldsrundans@gmail.com>
2017-11-21 19:19:52 +0200

    Update Multiply.cpp

--- Matrix/Multiply.cpp
+++ Matrix/Multiply.cpp
@@ -5,83 +5,93 @@ using namespace std;
 int main()
 {
     fstream f("in.txt",ios::in);
-    int n,t;
-    f>>n;
-    f>>t;
+    int r1,r2,c1,c2;
+     double n,t,sum;
     int i,j,k,l;
-    l=n*n;
-    double **A=new double *[n];
-    double **B=new double* [n];
-    double **C=new double *[n];
-
-
-    for(i=0;i<n;i++)
+    f>>r1;
+    f>>c1;
+    double **A=new double *[r1];
+    for(i=0;i<r1;i++)
     {
-        A[i]=new double[n];
-        B[i]=new double[n];
-        C[i]=new double[n];
+        A[i]=new double[c1];
     }
-    /*while(f)
+    for(i=0;i<r1;i++)
     {
-            cout<<t<<endl;
+        for(j=0;j<c1;j++)
+        {
             f>>t;
+            A[i][j]=t;
+        }
     }
-*/
-    for(i=0;i<n;i++)
+    f>>r2;
+    f>>c2;
+    double **B=new double *[r2];
+    for(i=0;i<r2;i++)
     {
-        for(j=0;j<n;j++)
+        B[i]=new double[c2];
+    }
+    for(i=0;i<r1;i++)
+    {
+        for(j=0;j<c1;j++)
+        {
+            f>>t;
+            B[i][j]=t;
+            cout<<"t="<<t<<" r2="<<r2<<endl;
+
+        }
+    }
+     f.close();
+    double **C=new double *[r2];
+    for(i=0;i<r2;i++)
+    {
+        C[i]=new double[c1];
+    }
+    for(i=0;i<r2;i++)
+    {
+        for(j=0;j<c1;j++)
         {
             C[i][j]=0;
         }
     }
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            A[i][j]=t;
-            f>>t;
-        }
-    }
-        for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            B[i][j]=t;
-            f>>t;
-        }
-    }
 
-
-    for(i=0;i<n;i++)
+    for(i=0;i<r1;i++)
     {
-        for(j=0;j<n;j++)
+        for(j=0;j<c2;j++)
         {
-             for(k=0;k<n;k++)
+            sum=0;
+             for(k=0;k<r2;k++)
              {
-                C[i][j]=C[i][j]+A[i][k]*B[k][j];
+             cout<<A[i][k]<<" ";
+             cout<<B[k][j]<<endl;
+                sum=sum+A[i][k]*B[k][j];
              }
+             C[i][j]=sum;
         }
     }
-//    mult(A,B,C,n);
- for(i=0;i<n;i++)
+ for(i=0;i<r2;i++)
     {
-        for(j=0;j<n;j++)
+        for(j=0;j<c1;j++)
         {
-            cout<<C[i][j];
+            cout<<C[i][j]<<" ";
         }
         cout<<endl;
     }
- for(i=0;i<n;i++)
+ for(i=0;i<r1;i++)
     {
-        delete [] A[i];
-        delete [] B[i];
+       delete [] A[i];
+      //  delete [] B[i];
+       // delete [] C[i];
+    }
+    //cout<<r1<<" "<<c1<<" "<<r2<<" "<<c2<<endl;
+    for(i=0;i<r2;i++)
+    {
+
         delete [] C[i];
+        delete [] B[i];
     }
     delete [] A;
     delete [] B;
     delete [] C;
-
-    f.close();
-   // cout << "Hello world!" << endl;
+  //  f.close();
     return 0;
 }
